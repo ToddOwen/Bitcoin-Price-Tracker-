0f8a5418d6e52b8b6ebe47f02e7004ed83b66f22
pleo7789 <todd7654@hotmail.co.uk>
2018-07-05 21:16:10 +0100

    Form Setup and Bitcoin Price  GET

---
+++ BitcoinPriceTracker/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace BitcoinPriceTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PriceText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PriceText
+            // 
+            this.PriceText.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.PriceText.AutoSize = true;
+            this.PriceText.Font = new System.Drawing.Font("Segoe UI Black", 42F, System.Drawing.FontStyle.Bold);
+            this.PriceText.ForeColor = System.Drawing.Color.DarkOrange;
+            this.PriceText.Location = new System.Drawing.Point(206, 49);
+            this.PriceText.Margin = new System.Windows.Forms.Padding(40);
+            this.PriceText.Name = "PriceText";
+            this.PriceText.Size = new System.Drawing.Size(294, 74);
+            this.PriceText.TabIndex = 0;
+            this.PriceText.Text = "PriceText";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(750, 200);
+            this.Controls.Add(this.PriceText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PriceText;
+    }
+}
+
---
+++ BitcoinPriceTracker/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+using Newtonsoft.Json;
+using System.Net;
+
+namespace BitcoinPriceTracker
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        BackgroundWorker CoinDesk, BlockChain;
+        public string CoinPrice;
+        public static string json;
+        public static decimal lastPrice = 0m, newPrice = 0m;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CoinDesk.RunWorkerAsync();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            CoinDesk = new BackgroundWorker();
+            CoinDesk.DoWork += new DoWorkEventHandler(CoinDeskData);
+
+        }
+
+        public void ChangeText(decimal lastPrice, decimal newPrice)
+        {
+            this.Invoke((MethodInvoker)delegate { PriceText.Text = CoinPrice; });
+            if (newPrice == lastPrice)
+                this.Invoke((MethodInvoker)delegate { PriceText.ForeColor = Color.DarkOrange; });
+            else if (newPrice > lastPrice)
+                this.Invoke((MethodInvoker)delegate { PriceText.ForeColor = Color.Red; });
+            else if (newPrice < lastPrice)
+                this.Invoke((MethodInvoker)delegate { PriceText.ForeColor = Color.Green; });
+        }
+
+        private void CoinDeskData(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                using (var web = new WebClient())
+                {
+                    var url = @"https://api.coindesk.com/v1/bpi/currentprice.json";
+                    json = web.DownloadString(url);
+
+                }
+
+                dynamic obj = JsonConvert.DeserializeObject(json);
+                var price = Convert.ToDecimal(obj.bpi.GBP.rate.Value);
+                newPrice = price;
+                CoinPrice = string.Format("1BTC = {0:C2}", newPrice);
+                ChangeText(lastPrice, newPrice);
+                lastPrice = newPrice;
+                Thread.Sleep(3000);
+            }
+
+        }
+
+
+    }
+}

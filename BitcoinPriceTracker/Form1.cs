d9cd3682fb4254346443bc7eb8aab618d1c66075
ToddOwen <todd7654@hotmail.co.uk>
2018-07-06 14:30:31 +0100

    added wallet tracker

--- BitcoinPriceTracker/Form1.cs
+++ BitcoinPriceTracker/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Microsoft.VisualBasic;
+
 
 
 namespace BitcoinPriceTracker
@@ -15,8 +17,9 @@ namespace BitcoinPriceTracker
     public partial class Form1 : Form
     {
 
-
-        public string CoinPrice;
+        public const string DefaultAddress = "1Lrdu3KHirkfTom5QZX9pGB6K2CPHvoebS";
+        public string CoinPrice, address = DefaultAddress;
+        DateTime lastChange, timeNow, walletUpdatedLast;
 
         public decimal lastPrice = 0m, newPrice = 0m;
 
@@ -29,15 +32,39 @@ namespace BitcoinPriceTracker
         {
             newPrice = await BitcoinTracking.GetPrice(newPrice);
             CoinPrice = string.Format("1 BTC = {0:C2}", newPrice);
-            
+            if (newPrice != lastPrice)
+            {
+                lastChange = DateTime.Now;
+            }
+
             ChangePrice(ref lastPrice, newPrice);
+            if (timeNow.AddMinutes(10) >= walletUpdatedLast)
+            {
+                updateWallet();
+                walletUpdatedLast = timeNow;
+            }
+        }
+
+        public async void updateWallet()
+        {
+            timeNow = DateTime.Now;
+            try
+            {
+                WalletText.Text = await BitcoinTracking.GetWalletValue(lastPrice, address);
+            }
+            catch (Exception e)
+            {
+                address = DefaultAddress;
+                MessageBox.Show(e.ToString());
+            }
             
         }
 
         public decimal ChangePrice(ref decimal lastPrice, decimal newPrice)
         {
             PriceText.Text = CoinPrice;
-            if (newPrice == lastPrice)
+            timeNow = DateTime.Now;
+            if (timeNow >= lastChange.AddSeconds(3))
             {
                 image.Image = Properties.Resources.if_Bitcoin_272914;
                 PriceText.ForeColor = Color.DarkOrange;
@@ -66,13 +93,17 @@ namespace BitcoinPriceTracker
         private bool mouseDown;
         private Point lastLocation;
 
+        private void WalletText_DoubleClick(object sender, EventArgs e)
+        {
+            address = Interaction.InputBox("Enter Wallet Address", "Wallet Address", address);
+            address = address.Trim();
+        }
+
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
             mouseDown = true;
             lastLocation = e.Location;
         }
-
-
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
